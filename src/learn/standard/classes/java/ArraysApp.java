c1405ecc07e9a54fd4a2d32cb6f13c8c9a7fb039
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-04-22 16:59:24 +0700

    done Arrays Class
    utilize copyOf, copyOfRange

--- src/learn/standard/classes/java/ArraysApp.java
+++ src/learn/standard/classes/java/ArraysApp.java
@@ -1,5 +1,7 @@
 package learn.standard.classes.java;
 
+import javax.print.Doc;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArraysApp {
@@ -9,7 +11,14 @@ public class ArraysApp {
         };
 
         Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(Arrays.binarySearch(numbers, 7));
+        System.out.println(Arrays.toString(numbers)); // [1, 7, 21, 37, 49, 63, 84, 98, 531, 542]
+        System.out.println(Arrays.binarySearch(numbers, 7)); // 1
+
+        int[] result = Arrays.copyOf(numbers, 5);
+        System.out.println(Arrays.toString(result)); // [1, 7, 21, 37, 49]
+
+        int[] result2 = Arrays.copyOfRange(numbers, 5, 10);
+        System.out.println(Arrays.toString(result2)); // [63, 84, 98, 531, 542]
+
     }
 }
